5be0a12fd8b3a5a5ccefc0493456c2684bb1c22e
SergeMar <marchenich@mail.ru>
2022-01-11 15:57:20 +0300

    Добавлены две функции

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -1,5 +1,17 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello("world");
+        hello("user");
+        hello("Sergey");
+        int s = 5;
+        System.out.println("Площадь квадрата со стороной "+s+" равна "+ area(s));
+    }
+
+    public static void hello(String hi) {
+        System.out.println("Hello, " + hi);
+    }
+
+    public static int area(int l){
+        return l*l;
     }
 }
